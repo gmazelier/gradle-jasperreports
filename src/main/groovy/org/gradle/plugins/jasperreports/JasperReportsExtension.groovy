5a86877a0d2bdd293c8138c76004e9d5e562c370
Gaylord Mazelier <gaylord@mazelier.fr>
2014-08-13 08:24:22 +0200

    Defines plugin extension with default values.

---
+++ src/main/groovy/org/gradle/plugins/jasperreports/JasperReportsExtension.groovy
@@ -0,0 +1,22 @@
+package org.gradle.plugins.jasperreports
+import org.gradle.api.Project
+
+class JasperReportsExtension {
+
+	def File srcDir = new File('src/main/jasperreports')
+	def File tmpDir = new File("${project.buildDir }/jasperreports")
+	def File outDir = new File("${project.buildDir }/classes/main")
+	def String srcExt = '.jrxml'
+	def String outExt = '.jasper'
+	def String compiler = 'net.sf.jasperreports.engine.design.JRJdtCompiler'
+	def boolean keepJava = false
+	def boolean validateXml = true
+	def boolean verbose = false
+
+	private Project project
+
+	JasperReportsExtension(Project project)  {
+		this.project = project
+	}
+
+}
