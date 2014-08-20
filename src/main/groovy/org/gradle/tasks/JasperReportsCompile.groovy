8a2706a86ab3bd3403450b0faa277d3c9de21416
Gaylord Mazelier <gaylord@mazelier.fr>
2014-08-20 08:28:19 +0200

    Adds exception to return value when compilation fails.

--- src/main/groovy/org/gradle/tasks/JasperReportsCompile.groovy
+++ src/main/groovy/org/gradle/tasks/JasperReportsCompile.groovy
@@ -46,7 +46,7 @@ class JasperReportsCompile extends DefaultTask {
 					JasperCompileManager.compileReportToFile(src.absolutePath, out.absolutePath);
 				} catch (any) {
 					log.lifecycle "An error occured: ${any.message}", any
-					return [name: src.name, success: false]
+					return [name: src.name, success: false, exception: any]
 				}
 				[name: src.name, success: true]
 			}
