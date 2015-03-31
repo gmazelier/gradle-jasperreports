b3c504326dc66aae046da90f4eb224f49e32b64c
Rankec <rankec2221@gmail.com>
2015-03-31 21:05:55 +0200

    added the possibility to use the java classpath as outPut dir

--- src/main/groovy/com/github/gmazelier/tasks/JasperReportsCompile.groovy
+++ src/main/groovy/com/github/gmazelier/tasks/JasperReportsCompile.groovy
@@ -1,92 +1,119 @@
 package com.github.gmazelier.tasks
 
-import static groovyx.gpars.GParsPool.withPool
-
 import net.sf.jasperreports.engine.JasperCompileManager
 import org.gradle.api.DefaultTask
 import org.gradle.api.GradleException
-import org.gradle.api.tasks.Input
-import org.gradle.api.tasks.InputDirectory
-import org.gradle.api.tasks.InputFiles
-import org.gradle.api.tasks.OutputDirectory
-import org.gradle.api.tasks.TaskAction
+import org.gradle.api.tasks.*
 import org.gradle.api.tasks.incremental.IncrementalTaskInputs
+
+import static groovyx.gpars.GParsPool.withPool
 
 class JasperReportsCompile extends DefaultTask {
 
-	@InputFiles Iterable<File> classpath
-	@InputDirectory def File srcDir
-	@OutputDirectory def File outDir
-	@Input def String srcExt
-	@Input def String outExt
-	def boolean verbose
+    @InputFiles
+    Iterable<File> classpath
+    @InputDirectory
+    def File srcDir
+    @OutputDirectory
+    def File outDir
+    @Input
+    def String srcExt
+    @Input
+    def String outExt
+    def boolean verbose
+    def boolean usePackageDir
+    def log = getLogger()
 
-	@TaskAction
-	void execute(IncrementalTaskInputs inputs) {
-		def log = getLogger()
+    @TaskAction
+    void execute(IncrementalTaskInputs inputs) {
 
-		def dependencies = classpath.collect { dependency ->
-			dependency?.toURI()?.toURL()
-		}
-    if (verbose) log.lifecycle "Additional classpath: ${dependencies}"
+        def dependencies = classpath.collect { dependency ->
+            dependency?.toURI()?.toURL()
+        }
+        if (verbose) log.lifecycle "Additional classpath: ${dependencies}"
 
-		def compilationTasks = []
-		inputs.outOfDate { change ->
-			if (change.file.name.endsWith(srcExt))
-				compilationTasks << [src: change.file, out: outputFile(change.file), deps: dependencies]
-		}
-		inputs.removed { change ->
-			if (verbose) log.lifecycle "Removed file ${change.file.name}"
-			def fileToRemove = outputFile(change.file)
-			fileToRemove.delete()
-		}
+        def compilationTasks = []
+        inputs.outOfDate { change ->
+            if (change.file.name.endsWith(srcExt))
+                compilationTasks << [src: change.file, out: outputFile(change.file), deps: dependencies]
+        }
+        inputs.removed { change ->
+            if (verbose) log.lifecycle "Removed file ${change.file.name}"
+            def fileToRemove = outputFile(change.file)
+            fileToRemove.delete()
+        }
 
-		def start = System.currentTimeMillis()
+        def start = System.currentTimeMillis()
 
-		def results = []
-		withPool {
-			results = compilationTasks.collectParallel { task ->
-				def src = task['src'] as File
-				def out = task['out'] as File
-				def deps = task['deps']
+        def results = []
+        withPool {
+            results = compilationTasks.collectParallel { task ->
+                def src = task['src'] as File
+                def out = task['out'] as File
+                def deps = task['deps']
 
-				if (verbose) log.lifecycle "Compiling file ${src.name}"
-				
-				try {
-					// Configure class loader with addtional dependencies
-					ClassLoader originalLoader = Thread.currentThread().getContextClassLoader()
-					URLClassLoader loader = new URLClassLoader(deps as URL[], originalLoader)
-					Thread.currentThread().setContextClassLoader loader
-					// Compile report
-					JasperCompileManager.compileReportToFile src.absolutePath, out.absolutePath
-					// Restore class loader
-					Thread.currentThread().setContextClassLoader originalLoader
-				} catch (any) {
-					return [name: src.name, success: false, exception: any]
-				}
-				[name: src.name, success: true]
-			}
-		}
+                if (verbose) log.lifecycle "Compiling file ${src.name}"
 
-		def stop = System.currentTimeMillis()
+                try {
+                    // Configure class loader with addtional dependencies
+                    ClassLoader originalLoader = Thread.currentThread().getContextClassLoader()
+                    URLClassLoader loader = new URLClassLoader(deps as URL[], originalLoader)
+                    Thread.currentThread().setContextClassLoader loader
+                    // Compile report
+                    JasperCompileManager.compileReportToFile src.absolutePath, out.absolutePath
+                    // Restore class loader
+                    Thread.currentThread().setContextClassLoader originalLoader
+                } catch (any) {
+                    return [name: src.name, success: false, exception: any]
+                }
+                [name: src.name, success: true]
+            }
+        }
 
-		def failures = results.findAll { !it['success'] }
-		if (failures) throw new GradleException(failureMessage(failures))
+        def stop = System.currentTimeMillis()
 
-		log.lifecycle "${results.size()} designs compiled in ${stop - start} ms"
-	}
+        def failures = results.findAll { !it['success'] }
+        if (failures) throw new GradleException(failureMessage(failures))
 
-	def File outputFile(File src) {
-		new File(outDir, src.name.replaceAll(srcExt, outExt))
-	}
+        log.lifecycle "${results.size()} designs compiled in ${stop - start} ms"
+    }
 
-	def failureMessage = { List failures ->
-		def stringBuilder = new StringBuilder()
-		stringBuilder.append "Could not compile ${failures.size()} designs:\n"
-		failures.each { failure ->
-			stringBuilder.append "\t[${failure['name']}] ${failure['exception'].message}\n"
-		}
-		stringBuilder.toString()
-	}
+    def File outputFile(File src) {
+
+        if (usePackageDir) {
+            def srcPackagePath = src.absolutePath.replaceAll(srcDir.absolutePath, "")
+            def outDirPath = outDir.absolutePath + srcPackagePath.replaceAll(src.name, "")
+            outDir = new File(outDirPath)
+            if (!outDir.isDirectory()) {
+                if (verbose) log.lifecycle "Create outDir: ${outDir.absolutePath}"
+                outDir.mkdirs()
+            }
+        }
+
+        /*def repla = src.absolutePath.replaceAll(srcDir.absolutePath, "")
+        println "replaced: ${repla}"
+        def dirPath = outDir.absolutePath + repla.replaceAll(src.name, "")
+        println "save to dir: ${dirPath}"
+        def tmpOutJasperDir = new File(dirPath)
+        if (tmpOutJasperDir.isDirectory()) {
+            println 'Directory already exsits'
+        } else {
+            tmpOutJasperDir.mkdirs()
+        }
+        */
+
+        //new File(tmpOutJasperDir, src.name.replaceAll(srcExt, outExt))
+        new File(outDir, src.name.replaceAll(srcExt, outExt))
+        //new File(outDir, repla.replaceAll(srcExt, outExt))
+    }
+
+    def failureMessage = { List failures ->
+        def stringBuilder = new StringBuilder()
+        stringBuilder.append "Could not compile ${failures.size()} designs:\n"
+        failures.each { failure ->
+            stringBuilder.append "\t[${failure['name']}] ${failure['exception'].message}\n"
+        }
+        stringBuilder.toString()
+    }
 
 }
--- src/main/groovy/com/github/gmazelier/tasks/JasperReportsPreCompile.groovy
+++ src/main/groovy/com/github/gmazelier/tasks/JasperReportsPreCompile.groovy
@@ -1,6 +1,4 @@
 package com.github.gmazelier.tasks
-import static net.sf.jasperreports.engine.design.JRCompiler.*
-import static net.sf.jasperreports.engine.xml.JRReportSaxParserFactory.COMPILER_XML_VALIDATION
 
 import net.sf.jasperreports.engine.DefaultJasperReportsContext
 import net.sf.jasperreports.engine.JasperReportsContext
@@ -8,81 +6,86 @@ import org.gradle.api.DefaultTask
 import org.gradle.api.InvalidUserDataException
 import org.gradle.api.tasks.TaskAction
 
+import static net.sf.jasperreports.engine.design.JRCompiler.*
+import static net.sf.jasperreports.engine.xml.JRReportSaxParserFactory.COMPILER_XML_VALIDATION
+
 class JasperReportsPreCompile extends DefaultTask {
 
-	def File srcDir
-	def File tmpDir
-	def File outDir
-	def String srcExt
-	def String outExt
-	def String compiler
-	def boolean keepJava
-	def boolean validateXml
-	def boolean verbose
+    def File srcDir
+    def File tmpDir
+    def File outDir
+    def String srcExt
+    def String outExt
+    def String compiler
+    def boolean keepJava
+    def boolean validateXml
+    def boolean verbose
+    def boolean usePackageDir
 
-	@TaskAction
-	void prepareCompilation() {
-		checkDirectories()
-		configureJasperReportsContext()
-		displayConfiguration()
-	}
+    @TaskAction
+    void prepareCompilation() {
+        checkDirectories()
+        configureJasperReportsContext()
+        displayConfiguration()
+    }
 
-	void checkDirectories() {
-		def Map<File,String> directoryErrors = [
-				(srcDir): false,
-				(tmpDir): true,
-				(outDir): true,
-		].collect { directory, isOutputDirectory ->
-			checkDirectory directory, isOutputDirectory
-		}.collectEntries().findAll { it.value }
+    void checkDirectories() {
+        def Map<File, String> directoryErrors = [
+                (srcDir): false,
+                (tmpDir): true,
+                (outDir): true,
+        ].collect { directory, isOutputDirectory ->
+            checkDirectory directory, isOutputDirectory
+        }.collectEntries().findAll { it.value }
 
-		if (directoryErrors) {
-			def message = directoryErrors.collect { directory, errorMessage ->
-				"${directory?.canonicalPath}: $errorMessage"
-			}.join ', '
-			throw new InvalidUserDataException(message)
-		}
-	}
+        if (directoryErrors) {
+            def message = directoryErrors.collect { directory, errorMessage ->
+                "${directory?.canonicalPath}: $errorMessage"
+            }.join ', '
+            throw new InvalidUserDataException(message)
+        }
+    }
 
-	void configureJasperReportsContext() {
-		JasperReportsContext context = DefaultJasperReportsContext.getInstance()
-		context.setProperty COMPILER_XML_VALIDATION, String.valueOf(validateXml)
-		context.setProperty COMPILER_PREFIX, compiler
-		context.setProperty COMPILER_KEEP_JAVA_FILE, String.valueOf(keepJava)
-		context.setProperty COMPILER_TEMP_DIR, tmpDir.canonicalPath
-	}
+    void configureJasperReportsContext() {
+        JasperReportsContext context = DefaultJasperReportsContext.getInstance()
+        context.setProperty COMPILER_XML_VALIDATION, String.valueOf(validateXml)
+        context.setProperty COMPILER_PREFIX, compiler
+        context.setProperty COMPILER_KEEP_JAVA_FILE, String.valueOf(keepJava)
+        context.setProperty COMPILER_TEMP_DIR, tmpDir.canonicalPath
+    }
 
-	def checkDirectory = { directory, isOutputDirectory ->
-		// If exists, it must be a directory
-		if (directory?.exists() && !directory.isDirectory())
-			return [directory, "${directory} is not a directory!"]
+    def checkDirectory = { directory, isOutputDirectory ->
+        // If exists, it must be a directory
+        if (directory?.exists() && !directory.isDirectory())
+            return [directory, "${directory} is not a directory!"]
 
-		// If is an output directory and does not exist, create it
-		if (isOutputDirectory && !directory.exists() && !directory.mkdirs())
-			return [directory, "${directory} cannot be create!"]
+        // If is an output directory and does not exist, create it
+        if (isOutputDirectory && !directory.exists() && !directory.mkdirs())
+            return [directory, "${directory} cannot be create!"]
 
-		// If is an output directory, it must be writable
-		if (isOutputDirectory && !directory.canWrite())
-			return [directory, "${directory} is not writeable!"]
+        // If is an output directory, it must be writable
+        if (isOutputDirectory && !directory.canWrite())
+            return [directory, "${directory} is not writeable!"]
 
-		[directory, null]
-	}
+        [directory, null]
+    }
 
-	void displayConfiguration() {
-		if (!verbose) return
+    void displayConfiguration() {
+        if (!verbose) return
 
-		getLogger().with {
-			lifecycle ">>> JasperReports Plugin Configuration"
-			lifecycle "Source directory: ${srcDir.canonicalPath}"
-			lifecycle "Temporary directory: ${tmpDir.canonicalPath}"
-			lifecycle "Output directory: ${outDir.canonicalPath}"
-			lifecycle "Source files extension: ${srcExt}"
-			lifecycle "Compiled files extension: ${outExt}"
-			lifecycle "Compiler: ${compiler}"
-			lifecycle "Keep Java files: ${keepJava}"
-			lifecycle "Validate XML before compiling: ${validateXml}"
-			lifecycle "<<<"
-		}
-	}
+        getLogger().with {
+            lifecycle ">>> JasperReports Plugin Configuration"
+            lifecycle "Source directory: ${srcDir.canonicalPath}"
+            lifecycle "Temporary directory: ${tmpDir.canonicalPath}"
+            lifecycle "Output directory: ${outDir.canonicalPath}"
+            lifecycle "Source files extension: ${srcExt}"
+            lifecycle "Compiled files extension: ${outExt}"
+            lifecycle "Compiler: ${compiler}"
+            lifecycle "Keep Java files: ${keepJava}"
+            lifecycle "Validate XML before compiling: ${validateXml}"
+            lifecycle "Use Java package dir: ${usePackageDir}"
+            lifecycle "<<<"
+        }
+    }
 
 }
