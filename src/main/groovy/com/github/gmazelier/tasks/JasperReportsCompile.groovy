d72f9de9099f29a55573de23de394f5c7720c3c6
Blake Jackson <blaketastic2@gmail.com>
2015-11-30 09:59:13 -0500

    code clean up
    removed logging that was not required

--- src/main/groovy/com/github/gmazelier/tasks/JasperReportsCompile.groovy
+++ src/main/groovy/com/github/gmazelier/tasks/JasperReportsCompile.groovy
@@ -89,10 +89,7 @@ class JasperReportsCompile extends DefaultTask {
 		if (useRelativeOutDir) {
 
 			Path srcPath = src.toPath()
-			log.lifecycle "srcPath: ${srcPath}"
 			Path srcDirPath = srcDir.getAbsoluteFile().toPath()
-			log.lifecycle "srcDirPath: ${srcDirPath}"
-
 			Path relativePath = srcDirPath.relativize(srcPath)
 			def parent = relativePath.parent != null ? relativePath.parent.toString() : ""
 			def path = Paths.get(useOutDir.absolutePath, parent)
